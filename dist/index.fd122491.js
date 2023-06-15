1cc29809080816999f8ac0ef4fb6831305d1df2c
KhanicaMao <khanica.mao@studentfr.ch>
2023-06-15 16:24:07 +0200

    Modification html et dossier

---
+++ dist/index.fd122491.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".button"),t=document.querySelector(".menu");e.addEventListener("click",(function(){t.classList.toggle("is-open"),e.classList.toggle("is-active")}));document.getElementById("menu");
+//# sourceMappingURL=index.fd122491.js.map
