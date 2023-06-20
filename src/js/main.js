1dda279468e21e4397330ed8b2f018bcf35791b2
KhanicaMao <khanica.mao@studentfr.ch>
2023-06-20 16:19:08 +0200

    Modification du index et animation éléments

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,74 @@
-var button = document.querySelector(".button");
-var menu = document.querySelector(".menu");
-var openMenu = function () {
-  menu.classList.toggle("is-open");
-  button.classList.toggle("is-active");
-};
+const button = document.querySelector(".button");
+const menu = document.querySelector(".menu");
 
-button.addEventListener("click", openMenu);
-
-var div = document.getElementById("menu");
-var display = 0;
-
-function hideShow() {
-  if (display == 1) {
-    div.style.display = "block";
-    display = 0;
+button.addEventListener("click", () => {
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
   } else {
-    div.style.display = "none";
-    display = "1";
+    menu.style.display = "none";
   }
-}
+});
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".box", {
+  scrollTrigger: {
+    trigger: ".box",
+    markers: false,
+    start: "top center",
+    end: "bottom center",
+  },
+  opacity: 0,
+  y: 100,
+  duration: 1.5,
+});
+
+gsap.from(".box-artiste", {
+  scrollTrigger: {
+    trigger: ".box-artiste",
+    markers: false,
+    start: "top center",
+    end: "bottom center",
+  },
+  opacity: 0,
+  y: 100,
+  duration: 1.5,
+});
+
+gsap.from(".box-jour1", {
+  scrollTrigger: {
+    trigger: ".box-jour1",
+    markers: false,
+    start: "top center",
+    end: "bottom center",
+  },
+  opacity: 0,
+  y: 100,
+  duration: 1.5,
+});
+
+gsap.from(".box-jour2", {
+  scrollTrigger: {
+    trigger: ".box-jour2",
+    markers: false,
+    start: "top center",
+    end: "bottom center",
+  },
+  opacity: 0,
+  y: 100,
+  duration: 1,
+});
+
+gsap.from(".box-jour3", {
+  scrollTrigger: {
+    trigger: ".box-jour3",
+    markers: false,
+    start: "top center",
+    end: "bottom center",
+  },
+  opacity: 0,
+  y: 100,
+  duration: 1,
+});
