3cd1fcbc04142f2cc95de860e9eb2887c0f610b1
KhanicaMao <khanica.mao@studentfr.ch>
2023-06-22 11:37:50 +0200

    Modification du css

--- dist/index.fd122491.js
+++
@@ -1,2 +0,0 @@
-var e=document.querySelector(".button"),t=document.querySelector(".menu");e.addEventListener("click",(function(){t.classList.toggle("is-open"),e.classList.toggle("is-active")}));document.getElementById("menu");
-//# sourceMappingURL=index.fd122491.js.map
